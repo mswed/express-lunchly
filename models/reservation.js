3940d1c3664f009d6d3dee950914e1902103aad1
Moshe Swed <mswed@beapot.com>
2024-07-04 23:15:19 -0400

    fullName function

--- models/reservation.js
+++ models/reservation.js
@@ -8,36 +8,36 @@ const db = require("../db");
 /** A reservation for a party */
 
 class Reservation {
-  constructor({id, customerId, numGuests, startAt, notes}) {
-    this.id = id;
-    this.customerId = customerId;
-    this.numGuests = numGuests;
-    this.startAt = startAt;
-    this.notes = notes;
-  }
+    constructor({id, customerId, numGuests, startAt, notes}) {
+        this.id = id;
+        this.customerId = customerId;
+        this.numGuests = numGuests;
+        this.startAt = startAt;
+        this.notes = notes;
+    }
 
-  /** formatter for startAt */
+    /** given a customer id, find their reservations. */
 
-  getformattedStartAt() {
-    return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
-  }
+    static async getReservationsForCustomer(customerId) {
+        const results = await db.query(
+            `SELECT id,
+                    customer_id AS "customerId",
+                    num_guests  AS "numGuests",
+                    start_at    AS "startAt",
+                    notes       AS "notes"
+             FROM reservations
+             WHERE customer_id = $1`,
+            [customerId]
+        );
 
-  /** given a customer id, find their reservations. */
+        return results.rows.map(row => new Reservation(row));
+    }
 
-  static async getReservationsForCustomer(customerId) {
-    const results = await db.query(
-          `SELECT id, 
-           customer_id AS "customerId", 
-           num_guests AS "numGuests", 
-           start_at AS "startAt", 
-           notes AS "notes"
-         FROM reservations 
-         WHERE customer_id = $1`,
-        [customerId]
-    );
+    /** formatter for startAt */
 
-    return results.rows.map(row => new Reservation(row));
-  }
+    getformattedStartAt() {
+        return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
+    }
 }
 
 
